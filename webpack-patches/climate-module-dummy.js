c4561516615efd181eca3df0cfc965ffea9e2ec5
Mohsendirbaz <dirbaz.sharif@gmail.com>
2025-05-30 11:07:21 -0500

    vercel 5/30/2025

--- webpack-patches/climate-module-dummy.js
+++
@@ -1,8 +0,0 @@
-// Dummy module to bypass missing ClimateModule import during build
-import React from 'react';
-
-const ClimateModule = () => {
-  return <div>Climate Module Placeholder</div>;
-};
-
-export default ClimateModule;
